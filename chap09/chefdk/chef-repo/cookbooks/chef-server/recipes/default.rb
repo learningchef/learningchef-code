5617ce5c251a9acbb2444d8a401869484e6c8b71
Mischa Taylor <mischa@misheska.com>
2014-07-17 21:58:33 -0500

    chef-server cookbook source

---
+++ chap09/chefdk/chef-repo/cookbooks/chef-server/recipes/default.rb
@@ -0,0 +1,28 @@
+# Cookbook Name:: chef-server
+# Recipe:: default
+#
+# Copyright (C) 2014
+#
+#
+#
+
+package_url = node['chef-server']['url']
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+# omnibus_package is remote (ie a URL) let's download it
+remote_file package_local_path do
+  source package_url
+end
+
+package package_name do
+  source package_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[reconfigure-chef-server]'
+end
+
+# reconfigure the installation
+execute 'reconfigure-chef-server' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
