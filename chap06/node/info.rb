ef3798f93329db8853d4d83caa01f8819c4f5977
Mischa Taylor <mischa@misheska.com>
2014-08-14 12:43:59 -0700

    Updating code for Chapter 6

--- chap06/node/info.rb
+++ chap06/node/info.rb
@@ -1,5 +1,7 @@
 log "IP Address: #{node['ipaddress']}"
 log "MAC Address: #{node['macaddress']}"
 log "OS Platform: #{node['platform']} #{node['platform_version']}"
-log "Running on a #{node['virtualization']['system']} #{node['virtualization']['role']}"
+log "Running on a #{node['virtualization']['system']} \
+#{node['virtualization']['role']}"
 log "Hostname: #{node['hostname']}"
+
