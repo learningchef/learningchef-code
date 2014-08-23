27ed11943856ddc978e47bb133f3677b529a2002
Mischa Taylor <mischa@misheska.com>
2014-08-22 21:24:18 -0400

    Adding example for Chapter 15

---
+++ chap15/chef-zero/cookbooks/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+default['apache']['document_root'] = '/var/www/html'
+default['apache']['port'] = 80
+default['motd']['message'] = 'Default message'
---
+++ chap15/chef-zero/cookbooks/apache/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright (C) 2014 
+#
+# 
+#
+
+package 'httpd'
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+# Add a template for Apache virtual host configuration
+template '/etc/httpd/conf.d/custom.conf' do
+  source 'custom.erb'
+  mode '0644'
+  variables(
+    :document_root => node['apache']['document_root'],
+    :port => node['apache']['port']
+  )
+  notifies :restart, 'service[httpd]'
+end
+
+document_root = node['apache']['document_root']
+
+# Add a directory resource to create the document_root
+directory document_root do
+  mode '0755'
+  recursive true
+end
+
+template "#{document_root}/index.html" do
+  source 'index.html.erb'
+  mode '0644'
+  variables(
+    :message => node['motd']['message'],
+    :port => node['apache']['port']
+  )
+end
