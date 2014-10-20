58928c60baaca8a506bb67e59f1ce0a0fb3e638c
Mischa Taylor <mischa@misheska.com>
2014-10-19 18:33:29 -0700

    Adding examples for Chapter 14

--- chap14/chef-playground/.chef/knife.rb
+++ chap14/chef-playground/.chef/knife.rb
@@ -5,4 +5,4 @@ node_name       "devhost"
 client_key      File.join(File.dirname(__FILE__), "devhost.pem")
 cookbook_path   "#{chef_repo}/cookbooks"
 cache_type      "BasicFile"
-cache_options   :path => "#{chef_repo}/checksums"
+cache_options   :path=>"#{chef_repo}/checksums"
--- chap14/chefdk/chef-playground/cookbooks/apache/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'apache'
-maintainer       'Mischa Taylor'
-maintainer_email 'mischa@misheska.com'
-license          'MIT'
-description      'Installs/Configures apache'
-long_description 'Installs/Configures apache'
-version          '0.1.0'
-
